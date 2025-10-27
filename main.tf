57361b78c7ed8d9d07e1f6a1c2c75cfacb23b7aa
amara200169 <Sheriffamara21@gmail.com>
2025-10-27 02:45:32 -0500

    Backend migration applied

--- main.tf
+++ main.tf
@@ -1,15 +1,11 @@
-# Remote state backend
 terraform {
   backend "s3" {
-    bucket         = "tf-state-aws-cost-optimizer-160564475451"  # Updated bucket name
+    bucket         = "sheri-aws-cost-tf-state-20251027"
     key            = "terraform.tfstate"
     region         = "us-east-2"
-    dynamodb_table = "tf-lock-aws-cost-optimizer"
     encrypt        = true
   }
-}
 
-terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -22,12 +18,72 @@ provider "aws" {
   region = "us-east-2"
 }
 
+# Zip up the lambda code from local folder
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "lambda_src"
   output_path = "handler.zip"
 }
 
+# IAM role that Lambda assumes
+resource "aws_iam_role" "lambda_role" {
+  name = "cost_optimizer_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach basic logging policy
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Custom inline policy for EC2 + CloudWatch + Cost Explorer
+resource "aws_iam_role_policy" "lambda_ec2_ce" {
+  name = "lambda_ec2_ce_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:StopInstances"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "cloudwatch:GetMetricStatistics"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ce:GetCostAndUsage"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Deploy Lambda function
 resource "aws_lambda_function" "optimizer" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "aws-cost-optimizer"
@@ -49,72 +105,20 @@ resource "aws_lambda_function" "optimizer" {
   }
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "cost_optimizer_lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy" "lambda_ec2_ce" {
-  name = "lambda_ec2_ce_policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:StopInstances"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "cloudwatch:GetMetricStatistics"
-        ]
-        Resource = "*"
-      },
-      {
-        Effect = "Allow"
-        Action = [
-          "ce:GetCostAndUsage"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
+# Schedule the Lambda daily via CloudWatch Events
 resource "aws_cloudwatch_event_rule" "daily_trigger" {
   name                = "daily-cost-scan"
-  schedule_expression = "cron(0 2 * * ? *)"
+  schedule_expression = "cron(0 2 * * ? *)" # 2:00 AM UTC daily
 }
 
+# Target the Lambda from the rule
 resource "aws_cloudwatch_event_target" "lambda_target" {
   rule      = aws_cloudwatch_event_rule.daily_trigger.name
   target_id = "optimizer-target"
   arn       = aws_lambda_function.optimizer.arn
 }
 
+# Allow EventBridge to invoke the Lambda
 resource "aws_lambda_permission" "allow_cloudwatch" {
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
@@ -123,6 +127,7 @@ resource "aws_lambda_permission" "allow_cloudwatch" {
   source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
 }
 
+# Output the Lambda ARN
 output "lambda_arn" {
   value = aws_lambda_function.optimizer.arn
 }
